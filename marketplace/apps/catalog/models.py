c7b61c6a9ea899e424b2ec02c19d90fe9e22a823
Kirill <kirill.1998@inbox.ru>
2023-01-05 16:53:03 +0300

    #1 спроектировал бд

---
+++ marketplace/apps/catalog/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category',on_delete=models.CASCADE,blank=True,null=True)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(blank=True)
+    description = models.TextField('описание', blank=True)
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,null=True,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    comment = models.TextField('Комментарий')
+    marks = models.IntegerField('Оценка', default=0)
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
---
+++ marketplace/apps/user/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Max
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField('Баланс', default=0, decimal_places=2, max_digits=9)
+    expired = models.DecimalField('Расходы', default=0, decimal_places=2, max_digits=9)
+    avatar = models.ImageField(blank=True)
+
+    @property
+    def status(self):
+        if self.expired <= 5000:
+            return 'basic'
+        elif 5000 <= self.expired <= 10000:
+            return 'gold'
+        elif self.expired >= 10000:
+            return 'premium'
+
+    def expired_add(self,price):
+        self.expired += price
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профиль'
